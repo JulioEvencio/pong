ed61d456b90426d9b39d6188094370c305fcff13
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-20 15:18:12 -0300

    Adiconada janela, tela e loop do jogo

--- main.c
+++ main.c
@@ -11,7 +11,46 @@
 
 int main(int argc, char *args[])
 {
-    //  Code
+    //  Iniciando SDl, IMG e TTF
+    sdl2_iniciar_SDL2();
+
+    //  Criando janela e tela
+    janela = sdl2_criar_janela(JANELA_NOME, JANELA_LARGURA, JANELA_ALTURA);
+    tela = sdl2_criar_tela(janela);
+
+    //  Loop do jogo
+    while(loop)
+    {
+        //  Verificando se algum evento ocorreu
+        while(SDL_PollEvent(&evento) != 0)
+        {
+            /*  Verificando qual evento ocorreu */
+            //  Usuario clicou no X da janela
+            if(evento.type == SDL_QUIT)
+            {
+                loop = 0;
+            }
+        }
+
+        /*  Logica */
+
+        /*  Graficos */
+        //  Definindo cor da tela
+        sdl2_definir_cor_tela(tela, JANELA_COR);
+
+        //  Atualizando tela
+        sdl2_atualizar_tela(tela);
+
+        /*  Delay */
+        SDL_Delay(JOGO_DELAY);
+    }
+
+    /*  Finalizando programa */
+    //  Fechando tela e janela
+    sdl2_fechar_tela(tela);
+    sdl2_fechar_janela(janela);
+    //  Finalizando SDl, IMG e TTF
+    sdl2_finalizar_SDL2();
 
     return 0;
 }
--- pong.h
+++ pong.h
@@ -5,10 +5,23 @@
 #include "sdl2/sdl2.h"
 
 /*  Constantes */
+//  Janela
+#define JANELA_NOME "Pong"
+#define JANELA_LARGURA 600
+#define JANELA_ALTURA 400
+#define JANELA_COR 255,255,255
+//  Jogo
+#define JOGO_DELAY 1000/60
 
 /*  Variaveis */
+int loop = 1;
 
 /*  Variaveis SDL */
+//  Janela e tela
+SDL_Window *janela = NULL;
+SDL_Renderer *tela = NULL;
+//  Evento
+SDL_Event evento;
 
 /*  Funcoes */
 
